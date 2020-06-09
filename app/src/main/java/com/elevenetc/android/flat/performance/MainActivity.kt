ae3058065cccede61c428bd2f6ccf5c3583229bd
“Eugene <shumvlesu@gmail.com>
2020-06-10 01:20:45 +0200

    Add sample plugin

--- app/src/main/java/com/elevenetc/android/flat/performance/MainActivity.kt
+++ app/src/main/java/com/elevenetc/android/flat/performance/MainActivity.kt
@@ -8,6 +8,7 @@ import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
+import com.abc.HelloClass
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
